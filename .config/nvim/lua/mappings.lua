0f13d3e1c522a78b60ea7a328d85d3f752aacc32
Zespre Schmidt <starbops@zespre.com>
2025-03-24 09:31:05 +0800

    Adjust nvim keybinding

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,3 +10,6 @@ map("i", "kj", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", "<leader><Tab><Tab>", "<cmd> set invlist <CR>")
+
+vim.keymap.del("n", "<leader>gt")
+map("n", "<leader>gs", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
